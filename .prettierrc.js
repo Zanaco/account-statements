769dc202f9140a29c759334e4c51beb4c8a56ed2
willymilimo <13065578+willymilimo@users.noreply.github.com>
2023-03-22 12:34:16 +0200

    added ma files

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    bracketSpacing: false,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    arrowParens: 'avoid',
+    semi: false,
+    tadWidth: 2,
+    tabs: true,
+  }
+  
